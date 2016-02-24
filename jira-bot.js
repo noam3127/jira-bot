e913d62c2a37043ee0e6d3e355f098a61a144fc9
Noam Lustiger <noshev27@gmail.com>
2016-02-23 20:06:29 -0500

    method for fetching commits

--- jira-bot.js
+++ jira-bot.js
@@ -47,10 +47,44 @@ module.exports = {
 
   getCommits: (req, res, next) => {
     let ticket = req.ticket;
-    let commitsUrl = 'https://monimus.atlassian.net/rest/dev-status/1.0/issue/summary?issueId=20802&_=1456263267288';
-    request(commitsUrl, (err, data, body) => {
-      console.log(data, body);
-      return res.json({data: body});
+    let commitsUrl = `https://monimus.atlassian.net/rest/dev-status/1.0/issue/detail?issueId=${ticket.id}&applicationType=github&dataType=repository&_=1456273044002`;
+    var encodedLogin = new Buffer(req.app.settings.loginCredentials).toString('base64');
+    let params = {
+      method: 'GET',
+      uri: commitsUrl,
+      headers: {
+        Authorization: `Basic ${encodedLogin}`,
+        'Content-Type': 'application/json'
+      }
+    };
+    request(params, (err, data, _body) => {
+      let body = JSON.parse(_body);
+      if (!body.detail.length || !body.detail[0].repositories || !body.detail[0].repositories.length) {
+        return res.json({text: `No commits found for ${ticket.key}`});
+      }
+
+      let commits = _.flatten(body.detail[0].repositories.map(repo => repo.commits));//.map(repo => repo.commits);
+      let response = {};
+      response.attachments = commits.map(commit => {
+        return {
+          text: `<${commit.url}|${commit.displayId}> ${commit.message} - ${commit.author.name}`
+        };
+      });
+
+      response.attachments[0].pretext = `${ticket.key}: ${ticket.fields.summary}`;
+
+      if (!req.body.response_url) {
+        res.set('Content-Type', 'application/json');
+        return res.status(200).json(response);
+      }
+
+      let responseParams = {
+        uri: req.body.response_url,
+        'Content-type': 'application/json',
+        body: JSON.stringify(response)
+      };
+
+      request.post(responseParams);
     });
   }
 };
